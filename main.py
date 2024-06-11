8582394dee195559a4ac2c378b3373770a80c3ab
zhuangxiaosen <624115642@qq.com>
2024-06-11 17:03:01 +0800

    完善跨平台路径.

--- main.py
+++ main.py
@@ -15,25 +15,26 @@ if __name__ == "__main__":
     current_directory = os.getcwd()
 
     # 加载配置
-    config_path = current_directory + f"\\config\\Testcase_config.json"
+    config_path = os.path.join(current_directory, 'config', 'Testcase_config.json')
     config_data = get_config(config_path)
     file_name = config_data["必填项"]["用例文件名"]
     if file_name == "":
         print("请在配置中填写文件名")
     template_name = config_data["必填项"]["模板文件名"]
 
-    xmind_file_path = current_directory + f"\\input\\{file_name}.xmind"
-    excel_file_path = current_directory + f"\\output\\{file_name}.xlsx"
-    template_file = current_directory + f"\\output\\{template_name}.xlsx"
-    output_dir = current_directory + f"\\output\\"
+    xmind_file_path = os.path.join(current_directory, 'input', f"{file_name}.xmind")
+    excel_file_path = os.path.join(current_directory, 'output', f"{file_name}.xlsx")
+    template_file = os.path.join(current_directory, 'output', f"{template_name}.xlsx")
+    output_dir = os.path.join(current_directory, 'output')
+    
     print(f"\n开始解析xmind文件，文件路径：{xmind_file_path}\n\n")
-    
     print(f"配置文件路径：{config_path}\n\n")
-    
+
     # 调用解析函数
     parsed_tree = xmind_to_tree.parse_xmind_to_tree(xmind_file_path)
 
     # 输出解析结果
     excel_output.process_to_excel(parsed_tree, excel_file_path, template_file, config_data)
+    
     print(f"转换已结束\n\n")
-    print(f"excel文件已输出到：{current_directory}\output\{file_name}.xlsx\n")
+    print(f"excel文件已输出到：{os.path.join(output_dir, f'{file_name}.xlsx')}\n")
